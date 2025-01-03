1f2ed654b5c6dc330ac4440a93b8e122344ea342
Ogak <78386672+Ogak-AI@users.noreply.github.com>
2025-01-03 21:59:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,24 @@ def index():
 @app.route('/download', methods=['POST'])
 def download_video():
     url = request.form['url']
-    ydl_opts = {'format': 'best'}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=False)
-        video_url = info_dict['url']
-    return video_url
+    
+    cookies_file = "/home/ec2-user/Demo_Youtube_Downloader/cookies.txt"
+
+    ydl_opts = {
+        'format': 'best',
+        'cookies': cookies_file,
+    }
+
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=False)
+            formats = info_dict.get('formats', [info_dict])
+            video_url = next(f['url'] for f in formats if f['format_id'] == 'best')
+        return redirect(video_url)
+    except youtube_dl.utils.DownloadError as e:
+        return str(e)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=80)
+
 
